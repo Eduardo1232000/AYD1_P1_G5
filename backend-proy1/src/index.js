96ca3cf7a3089cc04953b5dbf46f628461582490
Eduardo Reyes <eduardoalex2000@hotmail.com>
2024-06-06 15:48:30 -0600

    [ADD]PRUEBAS UNITARIAS LOGIN Y REGISTRO + MODIFICACION ESTRUCTURA BACKEND

--- backend-proy1/src/index.js
+++ backend-proy1/src/index.js
@@ -1,25 +1,6 @@
-const express = require('express');
-const cors = require('cors'); 
-const morgan=require('morgan');     //VER PETICIONES EN CONSOLA
+const app = require('./app');
+const PORT = process.env.PORT || 8080;
 
-//SETTINGS
-const app = express();
-const PORT =8080;       //PUERTO A USAR
-
-//MIDDLEWARES
-app.use(morgan('dev'));
-app.use(cors());
-app.use(express.json({ limit: '50mb' }));
-
-//RUTAS
-app.use(require('./routes/task.routes'));
-app.use(require('./routes/task.routes-Eduardo'));
-app.use(require('./routes/task.routes-Gladys'));
-app.use(require('./routes/task.routes-Heidy'));
-app.use(require('./routes/task.routes-Kevin'));
-app.use(require('./routes/task.routes-Ruben'));
-
-//EMPEZANDO SERVIDOR
 app.listen(PORT, () => {
     console.log(`Servidor API en http://localhost:${PORT}`);
 });
