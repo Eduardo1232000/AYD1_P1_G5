27d808dc6e48906767ae621f8eacdea62810f073
rubenralda <rubenralda@gmail.com>
2024-06-06 22:20:36 -0600

    add: endpoint peliculas

--- backend-proy1/src/routes/task.routes-Ruben.js
+++ backend-proy1/src/routes/task.routes-Ruben.js
@@ -1,9 +1,21 @@
-const express = require('express');
-const pool = require('../database/db');         //BASE DE DATOS
+const express = require("express");
+const pool = require("../database/db");
 const router = express.Router();
 
-//SOLO SE AGREGO PARA PROBAR QUE FUNCIONAN LOS ARCHIVOS SEPARADOS
-router.get('/rub', (req,res) => { //SOLICITUD INICIAL (SIN PARAMETROS)
+const campos = ["titulo", "sinopsis", "precio_alquiler", "director", "year_estreno", "duracion", "genero", "imagen"]
+
+router.put("/peliculas/:titulo", (req, res) => {
+    const vals = []
+    let set_valores = ""
+    for (const key in req.body) {
+        if (Object.hasOwnProperty.call(req.body, key)) {
+            if (campos.includes(key)) {
+                vals.push(req.body[key])
+                set_valores += `${key}=?,`
+            }
+        }
+    }
+    pool.query(`update peliculas set ${set_valores.substring(0, set_valores.length - 1)} where titulo=?;`, [...vals, req.params.titulo])
     res.json({ message: 'Servidor en puerto 8080 por parte de Ruben' });
 });
 
