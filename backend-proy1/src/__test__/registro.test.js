4e3401646a883c08b1faf9e2acf472ea1086ee2a
Eduardo Reyes <eduardoalex2000@hotmail.com>
2024-06-11 09:33:17 -0600

    Correccion de prueba unitaria de login y registro, Se agrego link scrum retrospective a documentacion

--- backend-proy1/src/__test__/registro.test.js
+++ backend-proy1/src/__test__/registro.test.js
@@ -5,7 +5,7 @@ describe('POST /register', () => {
     it('Deberia devolver un JSON, un mensaje de que ya existe el usuario y un estado 400', async () => {
         const response = await request(app)
             .post('/register')
-            .send({ nombre: 'Eduardo',apellido: 'Reyes',genero:'M', correo: 'eduardo@gmail.com',fecha_nacimiento:'2000-02-28', password: 'Eduardo123'});
+            .send({ username: 'Eduardo',lastname: 'Reyes',gender:'M', email: 'eduardo@gmail.com',birthdate:'2000-02-28', password: 'Eduardo123'});
         expect(response.body.message).toBe('Usuario ya Existe')
     })
 })
